c10783b0f244658317ba7f058252f54f55eed5c0
Ondrej Famera <ondrej@famera.cz>
2016-03-17 23:37:47 +0100

    added explicit connection URI to libvirt - preparation for non-root fast-vm release

--- centos-7-hacks.sh
+++ centos-7-hacks.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## serial console grub2
-virt-edit -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /boot/grub2/grub.cfg -e "s/terminal_output console/serial --unit=0 --speed=115200 --word=8 --parity=no\nterminal_input serial\nterminal_output serial/; s/rhgb quiet/console=ttyS0,115200n8/"
-virt-edit -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /etc/default/grub -e "s/GRUB_TERMINAL_OUTPUT=\"console\"/GRUB_TERMINAL=\"serial\"\nGRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no\"/; s/rhgb quiet/console=ttyS0,115200n8/"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /boot/grub2/grub.cfg -e "s/terminal_output console/serial --unit=0 --speed=115200 --word=8 --parity=no\nterminal_input serial\nterminal_output serial/; s/rhgb quiet/console=ttyS0,115200n8/"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /etc/default/grub -e "s/GRUB_TERMINAL_OUTPUT=\"console\"/GRUB_TERMINAL=\"serial\"\nGRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no\"/; s/rhgb quiet/console=ttyS0,115200n8/"
 
 ## activate eth0 on boot
-virt-edit -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /etc/sysconfig/network-scripts/ifcfg-eth0 -e "s/ONBOOT=no/ONBOOT=yes/"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /etc/sysconfig/network-scripts/ifcfg-eth0 -e "s/ONBOOT=no/ONBOOT=yes/"
 
 ## hostname
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
-virt-edit -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /etc/hostname -e "s/.*/$VM_HOSTNAME/"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/c7vg/root_lv -m /dev/sda1:/boot /etc/hostname -e "s/.*/$VM_HOSTNAME/"
--- ubuntu-1404-hacks.sh
+++ ubuntu-1404-hacks.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ## serial console grub2
-virt-edit -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /boot/grub/grub.cfg -e "s/terminal_output gfxterm/serial --unit=0 --speed=115200 --word=8 --parity=no\nterminal_input serial\nterminal_output serial/; s/root_lv /root_lv console=ttyS0,115200n8 /"
-virt-edit -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /etc/default/grub -e "s/GRUB_TERMINAL_OUTPUT=\"console\"/GRUB_TERMINAL=\"serial\"\nGRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no\"/; s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8\"/"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /boot/grub/grub.cfg -e "s/terminal_output gfxterm/serial --unit=0 --speed=115200 --word=8 --parity=no\nterminal_input serial\nterminal_output serial/; s/root_lv /root_lv console=ttyS0,115200n8 /"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /etc/default/grub -e "s/GRUB_TERMINAL_OUTPUT=\"console\"/GRUB_TERMINAL=\"serial\"\nGRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no\"/; s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8\"/"
 
 ## configuration file for the serial console
 TTYS0_FILE=$(mktemp --suffix=ttyS0.conf)
@@ -13,12 +13,12 @@ stop on runlevel [!2345]
 respawn
 exec /sbin/getty -L 115200 ttyS0 vt102
 EOF
-virt-copy-in -d $VM_NAME $TTYS0_FILE /etc/init/
+virt-copy-in --connect qemu:///system -d $VM_NAME $TTYS0_FILE /etc/init/
 rm $TTYS0_FILE
 
 ## activate eth0 on boot
-virt-edit -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /etc/network/interfaces -e "s/inet loopback/inet loopback\nauto eth0\niface eth0 inet dhcp/"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /etc/network/interfaces -e "s/inet loopback/inet loopback\nauto eth0\niface eth0 inet dhcp/"
 
 ## hostname
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
-virt-edit -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /etc/hostname -e "s/.*/$VM_HOSTNAME/"
+virt-edit --connect qemu:///system -d $VM_NAME -m /dev/u1404vg/root_lv -m /dev/sda1:/boot /etc/hostname -e "s/.*/$VM_HOSTNAME/"
