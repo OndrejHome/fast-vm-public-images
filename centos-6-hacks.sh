1fb1be52569bf5b375b278cd76d04b8480d87cc7
Ondrej Famera <ondrej@famera.cz>
2016-03-26 18:39:23 +0100

    ENHANCEMENT: hack files for centos reworked into single guestfish call.     This significantly improve the VM creation speed (2x-4x faster than before)

--- centos-6-hacks.sh
+++ centos-6-hacks.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-
 ## using direct backend to avoid selinux issues on fedora for now
 export LIBGUESTFS_BACKEND=direct
-
-## serial console grub config
-virt-edit --connect qemu:///system -d $VM_NAME -m /dev/c6vg/root_lv -m /dev/sda1:/boot /boot/grub/grub.conf -e "s/default=0$/default=0 #edited\nserial --unit=0 --speed=115200 --word=8 --parity=no\nterminal --timeout=5 serial console/; s/^hiddenmenu/#hiddenmenu/; s/^splashimage/#splashimage/; s/rhgb quiet/console=ttyS0,115200n8/"
-
-## eth0 MAC address change + enable network on boot
-virt-edit --connect qemu:///system -d $VM_NAME -m /dev/c6vg/root_lv -m /dev/sda1:/boot /etc/sysconfig/network-scripts/ifcfg-eth0 -e "s/HWADDR=.*$/HWADDR=${vm_mac}/; s/ONBOOT=no/ONBOOT=yes/"
-
 ## hostname
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
-virt-edit --connect qemu:///system -d $VM_NAME -m /dev/c6vg/root_lv -m /dev/sda1:/boot /etc/sysconfig/network -e "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/"
+
+guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/c6vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
+sh /usr/sbin/load_policy
+mkdir /root/.ssh
+sh 'restorecon /root/.ssh/'
+sh 'sed -i "s/HWADDR=.*$/HWADDR=${vm_mac}/; s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-eth0'
+sh 'sed -i "s/default=0$/default=0 #edited\nserial --unit=0 --speed=115200 --word=8 --parity=no\nterminal --timeout=5 serial console/; s/^hiddenmenu/#hiddenmenu/; s/^splashimage/#splashimage/; s/rhgb quiet/console=ttyS0,115200n8/" /boot/grub/grub.conf'
+sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
+EOF
