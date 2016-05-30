e1aa112914ac3cb49ed6185b3cb9f57e3b9a720a
Ondrej Famera <ondrej@famera.cz>
2016-05-30 22:56:39 +0200

    add CentOS 5.xx xml definition and hack file

---
+++ centos-5-hacks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+## using direct backend to avoid selinux issues on fedora for now
+export LIBGUESTFS_BACKEND=direct
+## hostname
+VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
+
+guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/c5vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
+sh 'sed -i "s/default=0$/default=0 #edited\nserial --unit=0 --speed=115200 --word=8 --parity=no\nterminal --timeout=5 serial console/; s/^hiddenmenu/#hiddenmenu/; s/^splashimage/#splashimage/; s/root_lv$/root_lv console=ttyS0,115200n8/" /boot/grub/grub.conf'
+sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
+EOF
