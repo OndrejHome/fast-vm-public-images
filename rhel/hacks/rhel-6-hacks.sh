8a260c1f6740ad53d85e20a59c197fa78f1febd3
Ondrej Famera <ondrej@famera.cz>
2016-07-18 23:13:31 +0200

    remove creation of /root.ssh from centos/rhel hack files

--- rhel/hacks/rhel-6-hacks.sh
+++ rhel/hacks/rhel-6-hacks.sh
@@ -6,8 +6,6 @@ VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/r6vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
 sh /usr/sbin/load_policy
-mkdir /root/.ssh
-sh 'restorecon /root/.ssh/'
 sh 'sed -i "s/HWADDR=.*$/HWADDR=${VM_MAC}/; s/BOOTPROTO=.*//; s/ONBOOT=.*/ONBOOT=yes\nBOOTPROTO=dhcp/" /etc/sysconfig/network-scripts/ifcfg-eth0'
 sh 'sed -i "s/default=0$/default=0 #edited\nserial --unit=0 --speed=115200 --word=8 --parity=no\nterminal --timeout=5 serial console/; s/^hiddenmenu/#hiddenmenu/; s/^splashimage/#splashimage/; s/root_lv/root_lv console=ttyS0,115200n8/; s/ rhgb//; s/ quiet//" /boot/grub/grub.conf'
 sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
--- rhel/hacks/rhel-7-hacks.sh
+++ rhel/hacks/rhel-7-hacks.sh
@@ -6,8 +6,6 @@ VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/r7vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
 sh /usr/sbin/load_policy
-mkdir /root/.ssh
-sh 'restorecon /root/.ssh/'
 sh 'sed -i "s/HWADDR=.*$/HWADDR=${VM_MAC}/; s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-eth0'
 sh 'sed -i "s/terminal_output console/serial --unit=0 --speed=115200 --word=8 --parity=no\nterminal_input serial\nterminal_output serial/; s/root_lv/root_lv console=ttyS0,115200n8/; s/ rhgb//; s/ quiet//" /boot/grub2/grub.cfg'
 sh 'sed -i "s/GRUB_TERMINAL_OUTPUT=\"console\"/GRUB_TERMINAL=\"serial\"\nGRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no\"/; s/rhgb quiet/console=ttyS0,115200n8/" /etc/default/grub'
