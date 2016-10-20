41bc6a324e3db0d945f365724daffc772e6ba0ec
Ondrej Famera <ofamera@redhat.com>
2016-10-20 09:16:00 +0200

    FIX incorrect settings for Ubuntu 16.04 hack file

--- ubuntu/hacks/ubuntu-1604-hacks.sh
+++ ubuntu/hacks/ubuntu-1604-hacks.sh
@@ -4,11 +4,10 @@ export LIBGUESTFS_BACKEND=direct
 ## hostname
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
-guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/u1404vg/root_lv -m /dev/sda1:/boot <<EOF
+guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/u1604vg/root_lv -m /dev/sda1:/boot <<EOF
 
 sh 'sed -i "s/terminal_output gfxterm/serial --unit=0 --speed=115200 --word=8 --parity=no\nterminal_input serial\nterminal_output serial/; s/root_lv /root_lv console=ttyS0,115200n8 /" /boot/grub/grub.cfg'
 sh 'sed -i "s/GRUB_TERMINAL_OUTPUT=\"console\"/GRUB_TERMINAL=\"serial\"\nGRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no\"/; s/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"console=ttyS0,115200n8\"/" /etc/default/grub'
-copy-in $TTYS0_FILE /etc/init/
 sh 'sed -i "s/inet loopback/inet loopback\nauto ens3\niface ens3 inet dhcp/" /etc/network/interfaces'
 sh 'sed -i "s/.*/$VM_HOSTNAME/" /etc/hostname'
 EOF
