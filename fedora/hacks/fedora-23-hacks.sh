618f6dac93c40b3de86242b56e21160e2dc52de9
Ondrej Famera <ondrej@famera.cz>
2016-11-14 23:36:19 +0100

    TEMPORARY WORKAROUND: allow the /usr/sbin/load_policy to fail due to problems on F24 when running systems with older selinux policies

--- fedora/hacks/fedora-23-hacks.sh
+++ fedora/hacks/fedora-23-hacks.sh
@@ -5,7 +5,7 @@ export LIBGUESTFS_BACKEND=direct
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/f23/root_lv -m /dev/sda1:/boot --selinux <<EOF
-sh /usr/sbin/load_policy
+- sh /usr/sbin/load_policy
 sh 'sed -i "s/terminal_output console/serial --unit=0 --speed=115200 --word=8 --parity=no\nterminal_input serial\nterminal_output serial/; s/rhgb quiet/console=ttyS0,115200n8/" /boot/grub2/grub.cfg'
 sh 'sed -i "s/GRUB_TERMINAL_OUTPUT=\"console\"/GRUB_TERMINAL=\"serial\"\nGRUB_SERIAL_COMMAND=\"serial --speed=115200 --unit=0 --word=8 --parity=no\"/; s/rhgb quiet/console=ttyS0,115200n8/" /etc/default/grub'
 sh 'sed -i "s/.*/$VM_HOSTNAME/" /etc/hostname'
