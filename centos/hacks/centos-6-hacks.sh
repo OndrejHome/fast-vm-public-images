77deedb3f25529230a94e05ca36acf18f92ab4df
Ondrej Famera <ondrej@famera.cz>
2017-02-05 14:30:40 +0100

    FIX SELinux issues with labeling by using selinux-relabel instead of calling load of selinux policy in VM. Fixed in RHEL and CentOS hack files

--- centos/hacks/centos-6-hacks.sh
+++ centos/hacks/centos-6-hacks.sh
@@ -5,8 +5,10 @@ export LIBGUESTFS_BACKEND=direct
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/c6vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
-- sh /usr/sbin/load_policy
 sh 'sed -i "s/HWADDR=.*$/HWADDR=${VM_MAC}/; s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-eth0'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-eth0
 sh 'sed -i "s/default=0$/default=0 #edited\nserial --unit=0 --speed=115200 --word=8 --parity=no\nterminal --timeout=5 serial console/; s/^hiddenmenu/#hiddenmenu/; s/^splashimage/#splashimage/; s/rhgb quiet/console=ttyS0,115200n8/" /boot/grub/grub.conf'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /boot/grub/grub.conf
 sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network
 EOF
--- rhel/hacks/rhel-5-hacks.sh
+++ rhel/hacks/rhel-5-hacks.sh
@@ -6,5 +6,7 @@ VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/r5vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
 sh 'sed -i "s/default=0$/default=0 #edited\nserial --unit=0 --speed=115200 --word=8 --parity=no\nterminal --timeout=5 serial console/; s/^hiddenmenu/#hiddenmenu/; s/^splashimage/#splashimage/; s/root_lv$/root_lv console=ttyS0,115200n8/" /boot/grub/grub.conf'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /boot/grub/grub.conf
 sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network
 EOF
