728f6c14e4be058d26e8adf5e19e6833820a9382
Ondrej Famera <ondrej@famera.cz>
2017-05-17 23:20:13 +0200

    "modularize" the virt-install scripts for CentOS 6/7

--- centos/ks/virt-install-centos-6.9.sh
+++ centos/ks/virt-install-centos-6.9.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
-BASE_IMAGE_LV=/dev/vg_ssd/fastvm-cent-6.9
-ISO_LOCATION=/wd4tb/workdir/tmp/CentOS-6.9-x86_64-bin-DVD1.iso
+# example: ./$0 /dev/vg_ssd/fastvm-cent-6.3 /wd4tb/workdir/tmp/ISO/CentOS-6.3-x86_64-bin-DVD1.iso centos-6.ks
+BASE_IMAGE_LV="$1"
+ISO_LOCATION="$2"
+KICKSTART="$3"
 
 virt-install \
 --connect qemu:///system \
---name centos-6.9-install \
+--name centos-6-fastvm-install \
 --ram 1024 --cpu host-model-only --vcpus 2 \
 --network network=default,model=virtio,link_state=down,mac=52:54:00:00:be:ef \
 --os-type=linux  --os-variant=centos6.3 \
 --disk path=$BASE_IMAGE_LV,bus=scsi,discard=unmap,format=raw \
 --controller scsi,model=virtio-scsi \
 --location $ISO_LOCATION \
---extra-args="ks=file:/centos-6.9.ks nonet text console=ttyS0,115200n8" \
---initrd-inject=centos-6.9.ks \
+--extra-args="ks=file:/$KICKSTART nonet text console=ttyS0,115200n8" \
+--initrd-inject=$KICKSTART \
 --noreboot \
 --nographics
 
-virsh --connect qemu:///system undefine centos-6.9-install
+virsh --connect qemu:///system undefine centos-6-fastvm-install
