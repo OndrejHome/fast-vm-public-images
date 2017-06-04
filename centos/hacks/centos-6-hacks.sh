46bae35dcc8f283d1b46738105be542b4ea75ebf
Ondrej Famera <ondrej@famera.cz>
2017-06-04 22:12:21 +0200

    revert selinux-relabel autodetection from 39a544fe56eb6f117ae59c6d594bb6a99d94b0dc and 89c9c43f0085c45bfc4fb867bb4044dd3976c817

--- centos/hacks/centos-6-hacks.sh
+++ centos/hacks/centos-6-hacks.sh
@@ -10,27 +10,12 @@ export LIBGUESTFS_BACKEND=direct
 ## hostname
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
-## determine if we have selinuxrelabel feature in guestfish
-echo "[i] checking if selinuxrelabel is available"
-guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" 2>/dev/null <<EOF
-run
-feature-available selinuxrelabel
-EOF
-
-if [ "$?" -eq 0 ]; then
-read SELINUX_RELABEL <<EOF
-selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-eth0
-selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /boot/grub/grub.conf
-selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network
-EOF
-else
-	echo "[w] selinuxrelabel is NOT available, '/.autorelabel' file will be created on VM to fix SELINUX contexts"
-	SELINUX_RELABEL='touch /.autorelabel'
-fi
-
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/c6vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
 sh 'sed -i "s/HWADDR=.*$/HWADDR=${VM_MAC}/; s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-eth0'
 sh 'sed -i "s/default=0$/default=0 #edited\nserial --unit=0 --speed=115200 --word=8 --parity=no\nterminal --timeout=5 serial console/; s/^hiddenmenu/#hiddenmenu/; s/^splashimage/#splashimage/; s/rhgb quiet/console=ttyS0,115200n8/; s/crashkernel=auto/crashkernel=128M/" /boot/grub/grub.conf'
 sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
-$SELINUX_RELABEL
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/selinux/targeted/contexts/files/file_contexts
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-eth0
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /boot/grub/grub.conf
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network
 EOF
