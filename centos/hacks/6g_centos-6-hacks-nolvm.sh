9aa680264880e4c6af436f71beed0f80bf56783e
Ondrej Famera <ondrej@famera.cz>
2018-10-15 21:23:44 +0200

    add non-LVM versions of Centos/RHEL 6/7 kickstarts and hack files

---
+++ centos/hacks/6g_centos-6-hacks-nolvm.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+## test if guestfish command is present
+which guestfish 2>&1 > /dev/null
+if [ "$?" != 0 ]; then
+	echo "[!!!] Command 'guestfish' not found (Install it!). Making changes to VM FAILED."
+	exit 1
+fi
+## using direct backend to avoid selinux issues on fedora for now
+export LIBGUESTFS_BACKEND=direct
+## hostname
+VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
+
+guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/sda2 -m /dev/sda1:/boot <<EOF
+# configure correct MAC address for eth0 network adapter
+sh 'sed -i "s/HWADDR=.*$/HWADDR=\"${VM_MAC}\"/; s/^ONBOOT=.*$/ONBOOT=\"yes\"/" /etc/sysconfig/network-scripts/ifcfg-eth0'
+# change the hostname of machine
+sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
+# CentOS 6.4, 6.4, 6.5 contained broken line in 'file_contexts' file, this removes it so we can apply other selinux labels
+sh 'sed -i "/\\pid/d" /etc/selinux/targeted/contexts/files/file_contexts'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/selinux/targeted/contexts/files/file_contexts
+# relabel files that we were touching with correct SELinux labels
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-eth0
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network
+EOF
---
+++ centos/ks/centos-6-nolvm.ks
@@ -0,0 +1,34 @@
+#version=DEVEL
+install
+text
+cdrom
+lang en_US.UTF-8
+keyboard us
+network --onboot no --device eth0 --bootproto dhcp --noipv6
+rootpw  --plaintext testtest
+firewall --service=ssh
+authconfig --enableshadow --passalgo=sha512
+selinux --enforcing
+timezone --utc Europe/Prague
+bootloader --location=mbr --driveorder=sda --append="crashkernel=128M"
+# The following is the partition information you requested
+# Note that any partitions you deleted are not expressed
+# here so unless you clear all partitions first, this is
+# not guaranteed to work
+zerombr
+clearpart --none --drives=sda
+
+part /boot --fstype=ext4 --size=500
+part swap --fstype swap --size=256
+part / --fstype=ext4 --size=5000
+
+poweroff
+
+%packages --nobase
+@Core
+%end
+
+%post --nochroot
+/mnt/sysimage/sbin/fstrim /mnt/sysimage
+/mnt/sysimage/sbin/fstrim /mnt/sysimage/boot
+%end
