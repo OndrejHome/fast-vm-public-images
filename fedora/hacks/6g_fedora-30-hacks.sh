88b0470de108e57a573fc2bead0c78a04ab89556
Ondrej Famera <ondrej@famera.cz>
2019-04-03 22:01:43 +0900

    added Fedora 30 Server kickstart and hackfile
    based on testing of Fedora 30 Server BETA

---
+++ fedora/hacks/6g_fedora-30-hacks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## test if guestfish command is present
+which guestfish 2>&1 > /dev/null
+if [ "$?" != 0 ]; then
+	echo "[!!!] Command 'guestfish' not found (Install it!). Making changes to VM FAILED."
+	exit 1
+fi
+## using direct backend to avoid selinux issues on fedora for now
+export LIBGUESTFS_BACKEND=direct
+## hostname
+VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
+
+guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/f30/root_lv -m /dev/sda1:/boot --selinux <<EOF
+sh 'sed -i "s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-ens2'
+sh 'sed -i "s/.*/$VM_HOSTNAME/" /etc/hostname'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-ens2
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/hostname
+EOF
