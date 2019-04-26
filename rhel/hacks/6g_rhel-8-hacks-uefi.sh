53d3b9a09d9e6efc065edfca3478730a3b492bd7
Ondrej Famera <ondrej@famera.cz>
2019-04-26 11:24:17 +0200

    detect the interface number and rename the config if the network interface configuration created during install doesn't match the VM current assignment of network card
    Thanks to Apan and Nikola for reporting and testing

--- rhel/hacks/6g_rhel-8-hacks-uefi.sh
+++ rhel/hacks/6g_rhel-8-hacks-uefi.sh
@@ -19,11 +19,18 @@ if [ ! -f /etc/fedora-release ]; then
         fi
 fi
 
+# detect the slot where network card is as system uses hardware-based names for network interfaces
+net_card_slot=$(virsh --connect qemu:///system dumpxml $VM_NAME|xmllint --xpath "//interface[.//source[@network='$LIBVIRT_NETWORK']]/address/@slot" - 2>/dev/null|cut -d\" -f 2|head -1|cut -dx -f 2)
+net_card=$(($net_card_slot+0))
+
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/r8vg/root_lv -m /dev/sda2:/boot -m /dev/sda1:/boot/efi --selinux <<EOF
-# enable eth0 adapter on boot
-sh 'sed -i "s/ONBOOT=no/ONBOOT=yes/" /etc/sysconfig/network-scripts/ifcfg-ens2'
+# move the config from interface used during install to one assigned by libvirt as it may change (sometimes)
+# if there was no change this will report error that can be safely ignored
+-sh 'mv /etc/sysconfig/network-scripts/ifcfg-ens* /etc/sysconfig/network-scripts/ifcfg-ens${net_card}'
+# enable ensX adapter on boot and rename interface if needed
+sh 'sed -i "s/ONBOOT=no/ONBOOT=yes/; s/NAME=.*/NAME=ens${net_card}/; s/DEVICE=.*/DEVICE=ens${net_card}/" /etc/sysconfig/network-scripts/ifcfg-ens${net_card}'
 # change the hostname of machine
 sh 'sed -i "s/.*/$VM_HOSTNAME/" /etc/hostname'
-selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-ens2
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-ens${net_card}
 selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/hostname
 EOF
