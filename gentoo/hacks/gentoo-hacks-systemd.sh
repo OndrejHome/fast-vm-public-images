63d1a49ec238476313f6f39f245e6cd3e7621b80
Ondrej Famera <ondrej-xa2iel8u@famera.cz>
2019-06-10 14:17:49 +0900

    fix hardcoded netowrk name in gentoo systemd hack file

--- gentoo/hacks/gentoo-hacks-systemd.sh
+++ gentoo/hacks/gentoo-hacks-systemd.sh
@@ -5,7 +5,7 @@ export LIBGUESTFS_BACKEND=direct
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
 
 # detect the slot where network card is as system uses hardware-based names for network interfaces
-net_card_slot=$(virsh --connect qemu:///system dumpxml $VM_NAME|xmllint --xpath "//interface[.//source[@network='fast-vm-nat']]/address/@slot" - 2>/dev/null|cut -d\" -f 2|head -1|cut -dx -f 2)
+net_card_slot=$(virsh --connect qemu:///system dumpxml $VM_NAME|xmllint --xpath "//interface[.//source[@network='$LIBVIRT_NETWORK']]/address/@slot" - 2>/dev/null|cut -d\" -f 2|head -1|cut -dx -f 2)
 net_card=$(($net_card_slot+0))
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/sda3:/ -m /dev/sda1:/boot <<EOF
