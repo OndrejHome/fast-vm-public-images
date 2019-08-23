6c0b2d41a285ddaf88c3fd4c80afe516d183c7f6
Ondrej Famera <ondrej@famera.cz>
2019-08-23 12:57:35 +0900

    adjust timezone of VM to match hypervisor
    So far for the CentOS/RHEL/OL 6/7/8
    
    Thanks to @chhanz for inspiration.

--- ol/hacks/6g_ol-6-hacks.sh
+++ ol/hacks/6g_ol-6-hacks.sh
@@ -9,16 +9,22 @@ fi
 export LIBGUESTFS_BACKEND=direct
 ## hostname
 VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
+## timezone of hypervisor
+timezone=$(readlink /etc/localtime | sed 's/^.*zoneinfo\/\(.*\)$/\1/')
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/o6vg/root_lv -m /dev/sda1:/boot <<EOF
 # configure correct MAC address for eth0 network adapter
 sh 'sed -i "s/HWADDR=.*$/HWADDR=\"${VM_MAC}\"/; s/^ONBOOT=.*$/ONBOOT=\"yes\"/" /etc/sysconfig/network-scripts/ifcfg-eth0'
 # change the hostname of machine
 sh 'sed -i "s/HOSTNAME=.*$/HOSTNAME=$VM_HOSTNAME/" /etc/sysconfig/network'
+# change timezone of machine to match hypervisor
+sh 'rm -f /etc/localtime'
+sh 'ln -s /usr/share/zoneinfo/$timezone /etc/localtime'
 # RHEL 6.4, 6.4, 6.5 contained broken line in 'file_contexts' file, this removes it so we can apply other selinux labels
 sh 'sed -i "/\\pid/d" /etc/selinux/targeted/contexts/files/file_contexts'
 selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/selinux/targeted/contexts/files/file_contexts
 # relabel files that we were touching with correct SELinux labels
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/localtime
 selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-eth0
 selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network
 EOF
--- rhel/hacks/6g_rhel-8-hacks.sh
+++ rhel/hacks/6g_rhel-8-hacks.sh
@@ -22,6 +22,8 @@ fi
 # detect the slot where network card is as system uses hardware-based names for network interfaces
 net_card_slot=$(virsh --connect qemu:///system dumpxml $VM_NAME|xmllint --xpath "//interface[.//source[@network='$LIBVIRT_NETWORK']]/address/@slot" - 2>/dev/null|cut -d\" -f 2|head -1|cut -dx -f 2)
 net_card=$(($net_card_slot+0))
+## timezone of hypervisor
+timezone=$(readlink /etc/localtime | sed 's/^.*zoneinfo\/\(.*\)$/\1/')
 
 guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/r8vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
 # move the config from interface used during install to one assigned by libvirt as it may change (sometimes)
@@ -31,6 +33,10 @@ guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/r8vg/root_lv -m /dev/sda1:/boo
 sh 'sed -i "s/ONBOOT=no/ONBOOT=yes/; s/NAME=.*/NAME=ens${net_card}/; s/DEVICE=.*/DEVICE=ens${net_card}/" /etc/sysconfig/network-scripts/ifcfg-ens${net_card}'
 # change the hostname of machine
 sh 'sed -i "s/.*/$VM_HOSTNAME/" /etc/hostname'
+# change timezone of machine to match hypervisor
+sh 'rm -f /etc/localtime'
+sh 'ln -s /usr/share/zoneinfo/$timezone /etc/localtime'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/localtime
 selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-ens${net_card}
 selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/hostname
 EOF
