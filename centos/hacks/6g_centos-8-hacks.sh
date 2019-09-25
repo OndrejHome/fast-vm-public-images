90d0203d8256cabcc4e0a512d90e5ff23467e6d8
Ondrej Famera <ondrej@famera.cz>
2019-09-26 00:48:46 +0900

    Add files for CentOS 8.0
    BIOS and UEFI kickstarts, hackfiles and scripts for generating images
    
    as hinted in #10, files are very similar to ones for RHEL 8.0

---
+++ centos/hacks/6g_centos-8-hacks.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+## test if guestfish command is present
+which guestfish 2>&1 > /dev/null
+if [ "$?" != 0 ]; then
+	echo "[!!!] Command 'guestfish' not found (Install it!). Making changes to VM FAILED."
+	exit 1
+fi
+## using direct backend to avoid selinux issues on fedora for now
+export LIBGUESTFS_BACKEND=direct
+## hostname
+VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
+
+if [ ! -f /etc/fedora-release ]; then
+        if [ -d /var/tmp/fedora29/appliance ]; then
+                export LIBGUESTFS_PATH=/var/tmp/fedora29/appliance
+        else
+                echo "[!!!] Non-Fedora systems needs a Fedora 29 (or newer) appliance in /var/tmp/fedora29/appliance to run"
+                exit 1
+        fi
+fi
+
+# detect the slot where network card is as system uses hardware-based names for network interfaces
+net_card_slot=$(virsh --connect qemu:///system dumpxml $VM_NAME|xmllint --xpath "//interface[.//source[@network='$LIBVIRT_NETWORK']]/address/@slot" - 2>/dev/null|cut -d\" -f 2|head -1|cut -dx -f 2)
+net_card=$(($net_card_slot+0))
+## timezone of hypervisor
+timezone=$(readlink /etc/localtime | sed 's/^.*zoneinfo\/\(.*\)$/\1/')
+
+guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/c8vg/root_lv -m /dev/sda1:/boot --selinux <<EOF
+# move the config from interface used during install to one assigned by libvirt as it may change (sometimes)
+# if there was no change this will report error that can be safely ignored
+-sh 'mv /etc/sysconfig/network-scripts/ifcfg-ens* /etc/sysconfig/network-scripts/ifcfg-ens${net_card}'
+# enable ensX adapter on boot and rename interface if needed
+sh 'sed -i "s/ONBOOT=no/ONBOOT=yes/; s/NAME=.*/NAME=ens${net_card}/; s/DEVICE=.*/DEVICE=ens${net_card}/" /etc/sysconfig/network-scripts/ifcfg-ens${net_card}'
+# change the hostname of machine
+sh 'sed -i "s/.*/$VM_HOSTNAME/" /etc/hostname'
+# change timezone of machine to match hypervisor
+sh 'rm -f /etc/localtime'
+sh 'ln -s /usr/share/zoneinfo/$timezone /etc/localtime'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/localtime
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/sysconfig/network-scripts/ifcfg-ens${net_card}
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/hostname
+EOF
---
+++ centos/ks/virt-install-centos-8.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# example: ./$0 /dev/vg_ssd/fastvm-centos-8.0 /mnt/slow_large_data/ISO/CentOS-8-x86_64-1905-dvd1.iso centos-8.ks
+BASE_IMAGE_LV="$1"
+ISO_LOCATION="$2"
+KICKSTART="$3"
+
+virt-install \
+--connect qemu:///system \
+--name centos-8-fastvm-install \
+--ram 1024 --cpu host-model-only --vcpus 2 \
+--network network=default,model=virtio,link_state=down,mac=52:54:00:00:be:ef \
+--os-type=linux  --os-variant=rhel7.0 \
+--disk path=$BASE_IMAGE_LV,bus=scsi,discard=unmap,format=raw \
+--controller scsi,model=virtio-scsi \
+--location $ISO_LOCATION \
+--extra-args="ks=file:/$KICKSTART nonet text console=ttyS0,115200n8" \
+--initrd-inject=$KICKSTART \
+--noreboot \
+--nographics
+
+virsh --connect qemu:///system undefine centos-8-fastvm-install
