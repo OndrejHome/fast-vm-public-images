0404b5abbf0a4351b76291af5167fe439576639b
Ondrej Famera <ondrej@famera.cz>
2021-10-02 11:21:32 +0900

    RHEL 8.3+ requires use of "inst." prefix for kickstart cmdline - new virt-install script to reflect this

---
+++ rhel/ks/virt-install-rhel-8.3.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# example: ./$0 /dev/vg_ssd/fastvm-rhel-7.3 /wd4tb/workdir/tmp/ISO/rhel-server-7.3-x86_64-dvd.iso rhel-7.ks
+BASE_IMAGE_LV="$1"
+ISO_LOCATION="$2"
+KICKSTART="$3"
+
+virt-install \
+--connect qemu:///system \
+--name rhel-8-fastvm-install \
+--ram 1024 --cpu host-model-only --vcpus 2 \
+--network network=default,model=virtio,link_state=down,mac=52:54:00:00:be:ef \
+--os-type=linux  --os-variant=rhel7.0 \
+--disk path=$BASE_IMAGE_LV,bus=scsi,discard=unmap,format=raw \
+--controller scsi,model=virtio-scsi \
+--location $ISO_LOCATION \
+--extra-args="inst.ks=file:/$(basename $KICKSTART) nonet inst.text console=ttyS0,115200n8" \
+--initrd-inject=$KICKSTART \
+--noreboot \
+--nographics
+
+virsh --connect qemu:///system undefine rhel-8-fastvm-install
