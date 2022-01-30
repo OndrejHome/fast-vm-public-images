05c5f83cffa6dbd2399fdc6875b1b59230cb7237
Ondrej Famera <ondrej@famera.cz>
2022-01-30 15:10:03 +0900

    Add Rocky Linux files (kickstart, xml, hacks)

---
+++ rocky/ks/virt-install-rocky-8.4.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# example: ./$0 /dev/vg_ssd/fastvm-rocky-8.4 /wd4tb/workdir/tmp/ISO/Rocky-8.4-x86_64-dvd1.iso rocky-8.4.ks
+BASE_IMAGE_LV="$1"
+ISO_LOCATION="$2"
+KICKSTART="$3"
+
+virt-install \
+--connect qemu:///system \
+--name rocky-8-fastvm-install \
+--ram 1024 --cpu host-model-only --vcpus 2 \
+--network network=default,model=virtio,link_state=down,mac=52:54:00:00:be:ef \
+--os-type=linux  --os-variant=rhel7.0 \
+--disk path=$BASE_IMAGE_LV,bus=scsi,discard=unmap,format=raw \
+--controller scsi,model=virtio-scsi \
+--location $ISO_LOCATION \
+--extra-args="inst.ks=file:/$(basename $KICKSTART) nonet inst.text console=ttyS0,115200n8" \
+--initrd-inject=$KICKSTART \
+--noreboot \
+--nographics
+
+virsh --connect qemu:///system undefine rocky-8-fastvm-install
