8a9cf9724200db4c52865882e57fd681d89f7b65
Ondrej Famera <ondrej@famera.cz>
2022-11-17 20:03:37 +0900

    add RHEL 9 UEFI related files

---
+++ rhel/hacks/6g_rhel-9-hacks-uefi.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+## test if guestfish command is present
+which guestfish 2>&1 > /dev/null
+if [ "$?" != 0 ]; then
+	echo "[!!!] Command 'guestfish' not found (Install it!). Making changes to VM FAILED."
+	exit 1
+fi
+## using direct backend to avoid selinux issues on fedora for now
+export LIBGUESTFS_BACKEND=direct
+## hostname
+VM_HOSTNAME=$(echo $VM_NAME|sed -e 's/\./-/g; s/_/-/g')
+
+if [ ! -f /var/lib/fast-vm/appliance/capability_xfs_el9 ]; then
+        echo "[!!!] XFS_EL9 capable appliance required for proper run"
+        exit 1
+fi
+
+## timezone of hypervisor
+timezone=$(readlink /etc/localtime | sed 's/^.*zoneinfo\/\(.*\)$/\1/')
+
+guestfish -a "/dev/$THINPOOL_VG/$VM_NAME" -m /dev/r9vg/root_lv -m /dev/sda2:/boot -m /dev/sda1:/boot/efi --selinux <<EOF
+# change the hostname of machine
+sh 'sed -i "s/.*/$VM_HOSTNAME/" /etc/hostname'
+# allow SSH password login
+sh 'sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin yes/" /etc/ssh/sshd_config'
+# change timezone of machine to match hypervisor
+sh 'rm -f /etc/localtime'
+sh 'ln -s /usr/share/zoneinfo/$timezone /etc/localtime'
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/localtime
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/hostname
+selinux-relabel /etc/selinux/targeted/contexts/files/file_contexts /etc/ssh/sshd_config
+EOF
---
+++ rhel/ks/virt-install-rhel-9-uefi.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# example: ./$0 /dev/vg_ssd/fastvm-rhel-9.0-uefi /mnt/iso_storage/rhel/rhel-baseos-9.0-x86_64-dvd.iso rhel-9-uefi.ks
+BASE_IMAGE_LV="$1"
+ISO_LOCATION="$2"
+KICKSTART="$3"
+
+virt-install \
+--connect qemu:///system \
+--name rhel-9-uefi-fastvm-install \
+--ram 1024 --cpu host-model-only --vcpus 2 \
+--network network=default,model=virtio,link_state=down,mac=52:54:00:00:be:ef \
+--os-variant=rhel9.0 \
+--disk path=$BASE_IMAGE_LV,bus=scsi,discard=unmap,format=raw \
+--controller scsi,model=virtio-scsi \
+--location $ISO_LOCATION \
+--extra-args="inst.ks=file:/$(basename $KICKSTART) nonet inst.text console=ttyS0,115200n8" \
+--initrd-inject=$KICKSTART \
+--noreboot \
+--boot uefi \
+--nographics
+
+virsh --connect qemu:///system undefine --nvram rhel-9-uefi-fastvm-install
