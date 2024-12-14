fc052637d269a527dc195ebc5983b048362dc343
Ondrej Famera <ondrej@famera.cz>
2024-12-14 16:04:29 +0900

    add RHEL 10 related files based on testing from RHEL 10 Beta

---
+++ rhel/ks/virt-install-rhel-10.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# example: ./$0 /dev/vg_ssd/fastvm-rhel-9.0 /mnt/iso_storage/rhel/rhel-baseos-9.0-x86_64-dvd.iso rhel-9.ks
+BASE_IMAGE_LV="$1"
+ISO_LOCATION="$2"
+KICKSTART="$3"
+
+virt-install \
+--connect qemu:///system \
+--name rhel-10-fastvm-install \
+--ram 2048 --cpu host-model-only --vcpus 2 \
+--network network=default,model=virtio,link_state=down,mac=52:54:00:00:be:ef \
+--os-variant=rhel9.0 \
+--disk path=$BASE_IMAGE_LV,bus=scsi,discard=unmap,format=raw \
+--controller scsi,model=virtio-scsi \
+--location $ISO_LOCATION \
+--extra-args="inst.ks=file:/$(basename $KICKSTART) nonet inst.text console=ttyS0,115200n8" \
+--initrd-inject=$KICKSTART \
+--noreboot \
+--nographics
+
+virsh --connect qemu:///system undefine rhel-10-fastvm-install
